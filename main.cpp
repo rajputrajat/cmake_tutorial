d4030651e3bfc981995035e7fa96cdb09a2388f5
Rajat Rajput <rajputrajat@gmail.com>
2021-07-04 12:37:12 +0530

    add another source file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+float add(float, float);
+
 int main() {
-    std::cout << "hello cmake!" << std::endl;
+    std::cout << "hello cmake!" << "\n";
+    std::cout << add(10.0f, 20.2f) << "\n";
 }
