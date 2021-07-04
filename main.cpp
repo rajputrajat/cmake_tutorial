11937520af04045cd08f6c89eef34d10af2268fa
Rajat Rajput <rajputrajat@gmail.com>
2021-07-04 14:32:16 +0530

    correctly look for glfw lib. create window.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 #include <adder.h>
+#include <GLFW/glfw3.h>
 
 int main() {
     std::cout << "hello cmake!" << "\n";
     std::cout << add(10.0f, 20.2f) << "\n";
+    GLFWwindow *window;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Draw gears
+        // draw();
+
+        // Update animation
+        // animate();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    // Terminate GLFW
+    glfwTerminate();
 }
